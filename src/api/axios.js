174d7033589eabb722530399d2a0fc87d24178d8
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2026-01-15 01:23:44 +0500

    frontend bug

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // .env se read kare
+  baseURL: process.env.REACT_APP_API_URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
