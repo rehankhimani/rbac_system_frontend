8f97eaa755b33bff014121a6394510fd3a70d38c
rehankhimani <muhammadrehanabdulqadir@gmail.com>
2026-01-15 16:34:31 +0500

    for deploye updatessssss

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL + "/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-console.log(process.env.REACT_APP_API_URL)
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
